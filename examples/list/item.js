420201df8ebb9af18e60f0bc409a72f3970c3206
Philip Weaver <simplygreatwork@gmail.com>
2024-05-12 11:18:06 -0500

    shove

--- examples/list/item.js
+++ examples/list/item.js
@@ -3,18 +3,13 @@ import { Component } from 'component'
 
 export function _() {
 	
-	Component.ready(({ component, data, $ }) => {
+	Component.ready(({ component, data, $, elements }) => {
 		
 		const { item, link, bus } = data
-		const a = $('a.row')
-		const div = $('div.row')
-		a.href = link
-		a.innerText = item.title
-		div.onmousedown = () => {
-			window.location.hash = link
-		}
-		bus.on(`item-changed:${item.id}`, ({ item }) => {
-			a.innerText = item.title
-		})
+		const { div, a } = elements
+		
+		Object.assign(a, { href: link, innerText: item.title })
+		div.onmousedown = () => window.location.hash = link
+		bus.on(`item-changed:${item.id}`, ({ item }) => a.innerText = item.title)
 	})
 }
