8105f9efce1d8beb31fef2d9e2e378419fc7afc4
Philip Weaver <simplygreatwork@gmail.com>
2024-05-12 14:00:08 -0500

    shove

--- examples/tasks/task.js
+++ examples/tasks/task.js
@@ -5,20 +5,16 @@ export function _() {
 	
 	Component.ready(function({ component, data, $, elements }) {
 		
-		console.log(`elements: ${elements}`)
 		let { item, link, bus } = data
 		const div = $('div.row'), a = $('a.row')
-		// const { div, a } = elements
 		
 		Object.assign(a, { href: link, innerText: item.title })
 		div.onmousedown = () => window.location.hash = link
 		bus.on(`item-changed:${item.id}`, ({ item }) => {
 			a.innerText = item.title
 			item.done ? a.classList.add('done') : a.classList.remove('done')
-			if (item.closed) {
-				component.remove()
-				window.location.hash = `#/tasks`	
-			}
+			item.closed ? component.remove() : null
+			item.closed ? location.hash = `#/tasks` : null
 		})
 	})
 }
