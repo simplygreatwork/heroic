e3e5b655432c0e8ad09698a00d16044893186f23
Philip Weaver <simplygreatwork@gmail.com>
2024-05-15 17:20:51 -0500

    shove

--- modules/component/source/utility.js
+++ modules/component/source/utility.js
@@ -1,16 +1,12 @@
 
 export function find_top_level_component(component) {
 	
-	while (component.parent) {
-		component = component.parent
-	}
+	while (component.parent) component = component.parent
 	return component
 }
 
 export function walk_components(component, fn) {
 	
 	fn(component)
-	component.children.forEach(function(child) {
-		walk_components(child, fn)
-	})
+	component.children.forEach((child) => walk_components(child, fn))
 }
