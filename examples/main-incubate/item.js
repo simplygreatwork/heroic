281f7bd45e0b9fcf025eb23f88634b4cc668c729
Philip Weaver <simplygreatwork@gmail.com>
2024-05-23 03:06:57 -0500

    shove

--- examples/main-incubate/item.js
+++ examples/main-incubate/item.js
@@ -7,7 +7,7 @@ export function _() {
 		
 		const { item, link, selection } = data
 		const { div, a } = $()
-				
+		
 		Object.assign(a, { href: link, innerText: item.title })
 		div[1].onmousedown = () => {
 			selection.add(component)
