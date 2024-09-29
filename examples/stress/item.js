30b595e2a06b49e3975b28d67227b5ecde7fe929
Philip Weaver <simplygreatwork@gmail.com>
2024-09-29 00:59:22 -0500

    shove

--- examples/stress/item.js
+++ examples/stress/item.js
@@ -1,15 +1,15 @@
 
 import { Component } from 'component'
+import { bind } from 'bind'
 
 export function _() {
 	
 	Component.ready(function({ component, data, $ }) {
 		
-		const { item, link, bus } = data
+		const { item, link, bus, scope } = data
 		const { div, a } = $()
 		
 		Object.assign(a, { href: link, innerText: item.title })
-		div[1].onmousedown = () => window.location.hash = link
-		bus.on(`item-changed:${item.id}`, ({ item }) => a.innerText = item.title)
+		bind(div[1], 'mousedown', () => location.hash = link, scope)
 	})
 }
