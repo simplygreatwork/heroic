bc186d94ef79d7c5bf25f3715dd0de85cefeb895
Philip Weaver <simplygreatwork@gmail.com>
2024-10-02 00:59:41 -0500

    shove

--- examples/main/item.js
+++ examples/main/item.js
@@ -1,17 +1,18 @@
 
 import { Component } from 'component'
+import { bind } from 'bind'
 
 export function _() {
 	
 	Component.ready(({ component, data, $ }) => {
 		
-		const { item, link, selection } = data
+		const { item, link, selection, scope } = data
 		const { div, a } = $()
 		
 		Object.assign(a, { href: link, innerText: item.title })
-		div[1].onmousedown = () => {
+		bind(div[1], 'mousedown', () => {
+			location.hash = link
 			selection.add(component)
-			window.location.hash = link
-		}
+		}, scope)
 	})
 }
